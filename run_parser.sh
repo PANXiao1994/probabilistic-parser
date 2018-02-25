4c607f97142bbbd7824b6eb9feaff1a4570d3ee9
PANXiao1994 <panxiao1994@gmail.com>
2018-02-25 23:25:27 +0100

    2nd

--- run_parser.sh
+++ run_parser.sh
@@ -9,10 +9,8 @@ GOLDFILE="./data/test.trees"
 PARSEFILE="./results/test_parsed.trees"
 
 
-python2 ./commands/system.py $FILEDIR $TRAINFILE $TESTFILE $GOLDFILE 3099 5
+python2 ./commands/system.py $FILEDIR $TRAINFILE $TESTFILE $GOLDFILE $1 $2
 python2 ./commands/pcfg_learn.py $TRAINFILE $PRODDIR
 python2 ./commands/cykslover.py $PRODDIR $TESTFILE $PARSEFILE
 python2 ./commands/evals.py $PARSEFILE $GOLDFILE
-#python3 ./commands/preprocess.py $1 $2 $3 $PREPROCESSED
 
-#python2 ./commands/system.py $PREPROCESSED $CONTEXT2VECDIR $DICTDIR $4 $5
